d13b1e01a06e73aaeae8117bdf82b7645364d91b
Musafiri-88 <musafirijmi@gmail.com>
2026-02-12 12:02:22 +0200

    f-string python exercise main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,13 @@
-# Écrivez votre code ici !
+# exerces sur la modification et manipulation des variables et affichage avec la f-string
+# declaratio de variables
+nom_complet = "Jean-Mare Imani"
+age = 22
+
+# affichage avec f-string
+print(f"Je m'appelle {nom_complet} et j'ai {age} ans")
+
+# modification de la variable age
+age = age + 10
+
+# affichage avec f-string avec la nouvelle valeur de age
+print(f"Je m'appelle {nom_complet} et j'ai {age} ans")# Écrivez votre code ici !
